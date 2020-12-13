f475f5966333dea4e34564b1715116a2bb831481
Sam <me@samuelmessing.com>
2020-12-12 23:07:30 -0500

    day 2 part 2.

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -4,18 +4,38 @@ use std::path::Path;
 use regex::Regex;
 
 #[derive(Debug)]
-struct PasswordPolicy {
+struct OldPasswordPolicy {
     letter: char,
     min_times: i32,
     max_times: i32,
 }
 
-impl PasswordPolicy {
+impl OldPasswordPolicy {
     fn is_valid(&self, password: &String) -> bool {
         let num_times = (password.len() - password.replace(self.letter, "").len()) as i32;
         return self.max_times >= num_times && num_times >= self.min_times;
     }
 }
+
+#[derive(Debug)]
+struct PasswordPolicy {
+    letter: char,
+    index_a: usize,
+    index_b: usize,
+}
+
+impl PasswordPolicy {
+    fn is_valid(&self, password: &String) -> bool {
+        if self.index_a >= password.len() || self.index_b >= password.len() {
+            return false;
+        }
+        let first_char = password.as_bytes()[self.index_a] as char;
+        let second_char = password.as_bytes()[self.index_b] as char;
+        return (self.letter == first_char) ^ (self.letter == second_char);
+
+    }
+}
+
 
 fn main() {
     let mut passwords_and_policies = Vec::new();
@@ -25,25 +45,39 @@ fn main() {
         for line in lines {
             if let Ok(ip) = line {
                 for cap in re.captures_iter(&ip) {
-                    let policy = PasswordPolicy {
+                    let old_policy = OldPasswordPolicy {
                         letter: cap[3].parse::<char>().unwrap(),
                         min_times: cap[1].parse::<i32>().unwrap(),
                         max_times: cap[2].parse::<i32>().unwrap(),
                     };
+                    let new_policy = PasswordPolicy {
+                        letter: cap[3].parse::<char>().unwrap(),
+                        index_a: cap[1].parse::<usize>().unwrap() - 1,
+                        index_b: cap[2].parse::<usize>().unwrap() - 1,
+                    };
                     passwords_and_policies.push(
-                        (policy, cap[4].to_string()));
+                        (old_policy, new_policy, cap[4].to_string()));
                 }
             }
         }
     }
 
     let mut num_valid = 0;
-    for (policy, password) in &passwords_and_policies {
+    for (policy, _, password) in &passwords_and_policies {
         if policy.is_valid(password) {
             num_valid += 1;
         }
     }
     println!("The answer to part one is: {}", num_valid);
+
+    let mut num_new_valid = 0;
+    for (_, policy, password) in &passwords_and_policies {
+        if policy.is_valid(password) {
+            num_new_valid += 1;
+        }
+    }
+    println!("The answer to part one is: {}", num_new_valid);
+
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
